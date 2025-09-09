7ee67b03eaecfa974755b5cab7a59bb7226ec126
strazensdigital <strazensdigital@gmail.com>
2025-09-09 09:23:22 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
-// src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
-// Vercel Analytics (for pageviews, referrers, geo, etc.)
 import { Analytics } from "@vercel/analytics/react";
-// Vercel Speed Insights (Core Web Vitals & perf)
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { ErrorBoundary } from "./ErrorBoundary";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <Analytics />
-    <SpeedInsights />
+    <ErrorBoundary>
+      <App />
+      <Analytics />
+      <SpeedInsights />
+    </ErrorBoundary>
   </React.StrictMode>
 );
