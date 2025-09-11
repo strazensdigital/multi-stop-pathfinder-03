da0dfc2b4c826504bef80a69ef32ae0e79e59fa7
strazensdigital <strazensdigital@gmail.com>
2025-09-10 22:50:46 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import AuthCallback from "@/pages/AuthCallback";
+import LoginModal from "@/components/LoginModal";
 
 const queryClient = new QueryClient();
 
