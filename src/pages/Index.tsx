9ae96a730144dfcd4d9e140e6b8436e26448fa73
strazensdigital <strazensdigital@gmail.com>
2025-09-10 21:50:46 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,9 +7,7 @@ const Index = () => {
     <div className="min-h-screen">
       <header className="py-8">
          <div className="container mx-auto flex justify-end">
-            <button onClick={() => window.dispatchEvent(new CustomEvent('open-account-drawer', { detail:{ view:'plan'} }))} className="border rounded px-3 py-2 text-sm">
-            Account
-            </button>
+
           </div>
       </header>
       <main className="container mx-auto pb-16 space-y-8">
