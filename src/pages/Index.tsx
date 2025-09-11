428fdc8c6dfacab5131c651631798aa9956a7766
strazensdigital <strazensdigital@gmail.com>
2025-09-10 22:56:41 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,11 +5,7 @@ import AccountSheet from "@/components/AccountSheet";
 const Index = () => {
   return (
     <div className="min-h-screen">
-      <header className="py-8">
-         <div className="container mx-auto flex justify-end">
-
-          </div>
-      </header>
+      <Header />
       <main className="container mx-auto pb-16 space-y-8">
         <AccountSheet />
         <MapboxRoutePlanner />
